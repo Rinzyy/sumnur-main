4bd9931b737b9f75f280bac049d3f82311eb295c
Rinzy <atermizor@gmail.com>
2023-01-27 09:51:28 -0600

    api error on build

--- pages/api/AI/Email/Formal.ts
+++ pages/api/AI/Email/Formal.ts
@@ -3,7 +3,7 @@ import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
 	organization: 'org-i5ZJjyWAyhGgofxS14HjKmEZ',
-	apiKey: process.env.OPENAI_API_KEY,
+	apiKey: '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',
 });
 export const openai = new OpenAIApi(configuration);
 
